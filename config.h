0be5135a46668275e4e14f2ee1fd8f44d944ab1d
saito2 <saito2>
2003-06-22 08:16:19 +0000

    Remove VC++ project files.

--- config.h
+++
@@ -1,3 +0,0 @@
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
-
-#include "configs/msc-config.h"
--- interface.h
+++
@@ -1,7 +0,0 @@
-/* #define TWSYNG32 */
-
-#ifdef TWSYNG32
-#include "configs/msc-twsyn-interface.h"
-#else
-#include "configs/msc-interface.h"
-#endif
